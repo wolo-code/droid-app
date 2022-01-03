af87391126b8549482263b18142095e943c665f5
Ujjwal Singh <ujjwalsingh@outlook.com>
2022-01-03 12:32:44 +0530

    Fixed blank query from geo url issue

--- app/src/main/java/codes/wolo/droid/ForwardIntentGeoActivity.java
+++ app/src/main/java/codes/wolo/droid/ForwardIntentGeoActivity.java
@@ -1,13 +1,15 @@
 package codes.wolo.droid;
 
 import android.net.Uri;
+import android.net.UrlQuerySanitizer;
 
 public class ForwardIntentGeoActivity extends com.google.androidbrowserhelper.trusted.LauncherActivity {
 
     @Override
     protected Uri getLaunchingUrl() {
         Uri uri = this.getIntent().getData();
-        String query = uri.getQuery();
-        return (Uri.parse("https://wolo.codes/"+'?'+query));
+        String query = new UrlQuerySanitizer(uri.toString()).getValue("q");
+        return (Uri.parse("https://wolo.codes/"+'?'+"q="+query));
     }
+
 }
